1cd947e4e033aac9a852bdd1b70bcd4e3258f1a9
anvayabn <an001@ucr.edu>
2024-01-17 12:15:55 -0800

    added test ceiling function

--- src/bin_search.rs
+++ src/bin_search.rs
@@ -27,6 +27,37 @@ pub fn binary_search(vec: &mut Vec<i32> , target: i32) -> usize {
     1000 
 } 
 
+mod ceiling{
+
+    /* return the target number or the greatest  */
+    pub fn ceil(vec: &mut Vec<i32> , target: i32)->i32{ 
+
+        // first sort the vector 
+        vec.sort(); 
+
+        //if the target number is not present 
+        if target > vec[vec.len() - 1]{
+            return -1;
+        }
+
+        let mut i = 0; 
+        let mut j = (vec.len() - 1 ) as i32;
+        
+        while i <= j {
+
+            let mid = i + (j - i)/2 ;
+            if  vec[mid as usize] < target {
+                i = mid + 1;
+            }else if vec[mid as usize] > target {
+                j = mid - 1; 
+            }else {
+                return mid;
+            }
+        }
+        i as i32 
+
+    }
+}
 mod test{
     use super::*;
     #[test]
@@ -38,5 +69,14 @@ mod test{
 
         assert_eq!(binary_search(&mut nums, target), ans); 
 
+    }
+
+    #[test]
+    fn test_ceil(){
+        let mut nums = vec![1,2,4,5,6,7,8,10,22]; 
+        let target = 9; 
+        let ans = 7; 
+
+        assert_eq!(ceiling::ceil(&mut nums, target), ans); 
     }
 }
